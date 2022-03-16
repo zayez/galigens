f0263a7ddae14319e3712f12362d0023f33b7a5a
zayez <zayezuno@protonmail.com>
2022-03-15 22:42:17 -0300

    Switch to HashRouter

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom"
 import Home from "../pages/Home"
 import Rovers from "../components/Rovers"
 import Rover from "../components/Rover"
