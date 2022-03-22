d5f8d38bd1265efe2cb1fd702cc194e66f916f69
zayez <zayezuno@protonmail.com>
2022-03-22 17:59:37 -0300

    webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,11 +2,14 @@ const { merge } = require("webpack-merge")
 
 const common = require("./webpack.common.js")
 
-module.exports = merge(common, {
-  mode: "development",
-  devtool: "source-map",
-  devServer: {
-    static: "./dist",
-    historyApiFallback: true,
-  },
-})
+module.exports = (env, argv) => {
+  return merge(common, {
+    // mode: "none",
+    mode: "development",
+    devtool: "source-map",
+    devServer: {
+      static: "./dist",
+      historyApiFallback: true,
+    },
+  })
+}
