4a547c577c660d301f8a6776a1598450f1c7dd6e
zayez <zayezuno@protonmail.com>
2022-03-26 10:39:04 -0300

    Add some container classes

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const ErrorPage = () => {
   return (
-    <div className="error-page">
+    <div className="error-page container content-main">
       <h1> Error</h1>
       <p>Page not found.</p>
     </div>
