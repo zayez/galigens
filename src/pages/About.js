1616eba29a07b0ec33ee51b710e4a1521c6344fc
zayez <zayezuno@protonmail.com>
2022-03-26 10:48:12 -0300

    Add dummy about page

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const About = ({}) => {
+  return (
+    <div className="about container content-main">
+      <h2>About</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+        dolorum labore natus. Dolorem dolorum accusantium in cumque itaque
+        quisquam libero neque aspernatur modi, tempore repudiandae odit
+        quibusdam veniam, officia quasi?
+      </p>
+    </div>
+  )
+}
+
+export default About
