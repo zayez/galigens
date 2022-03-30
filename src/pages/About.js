12178518ac4e11fa77c39f18f47e7f7ddae1833c
zayez <zayezuno@protonmail.com>
2022-03-30 13:31:22 -0300

    Add link color

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,27 +3,41 @@ import React from "react"
 const About = ({}) => {
   return (
     <div className="about container content-main">
-      <h2 className="heading">About</h2>
+      <h1 className="heading">About</h1>
       <p className="about-text">
-        A webapp to explore the Martian surface using real-life photos from
+        <strong>
+          <em>Galigens</em>
+        </strong>{" "}
+        is a webapp to explore the Martian surface using real-life photos from
         Mars; captured by NASA's rovers operating on the Red Planet.
       </p>
-      <h3 className="sub-heading">Available rovers</h3>
+
+      <h2 className="sub-heading">Available rovers</h2>
       <ul className="about-list">
         <li>Spirit</li>
         <li>Oportunity</li>
         <li>Curiosity (active)</li>
         <li>Perseverance (active)</li>
       </ul>
-      <h3 className="sub-heading">Features</h3>
+      <h2 className="sub-heading">Features</h2>
       <ul className="about-list">
         <li>Display a list of available rovers with their mission details.</li>
         <li>Display a gallery of photos by selected rover.</li>
         <li>Allow to filter the photo gallery by date (or sol) and camera.</li>
         <li>Allow to zoom photos in the rover's photo gallery.</li>
       </ul>
+      <h2 className="sub-heading">License</h2>
+      <p>
+        <em>Galigens</em> is open-source and is distributed under the terms of
+        the MIT license. <br />
+        To know more, visit the{" "}
+        <a href="https://github.com/zayez/galigens" className="link">
+          Github
+        </a>{" "}
+        page.
+      </p>
       <h3 className="sub-heading">References</h3>
-      <ul className="about-list">
+      <ul className="about-list links">
         <li>
           <a href="https://mars.nasa.gov/msl/home/">
             Mars Curiosity Rover mission
@@ -46,7 +60,7 @@ const About = ({}) => {
         </li>
       </ul>
       <h3 className="sub-heading">Third-party resources</h3>
-      <ul className="about-list">
+      <ul className="about-list links">
         <li>
           <a href="https://api.nasa.gov/">NASA Open APIs</a>
         </li>
